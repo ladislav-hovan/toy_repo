1a5f99c79d5c9eb25db9bec3b4aab0e510c45826
Laco Hovan <19410951+ladislav-hovan@users.noreply.github.com>
2023-12-11 13:58:25 +0100

    Added print and a new function

--- something.py
+++ something.py
@@ -1,4 +1,8 @@
 import numpy as np
 
 def dice_rolls(n):
-    return np.random.randint(1,7,10)
+    print (f'The results of {n} dice rolls are:')
+    return np.random.randint(1,7,n)
+
+def d20_dice_rolls(n):
+    return np.random.randint(1,21,n)
